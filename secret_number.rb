66f0f1e6af5c37a9b41b6efdf1d75694fae06a37
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-06 12:09:03 -0400

    Finishes the code for the rest of the game

--- secret_number.rb
+++ secret_number.rb
@@ -7,3 +7,11 @@ while guess != secret_number
     guess = gets.chomp.to_i
     if guess > 10 OR guess < 1
         puts "Please enter a number between 1 and 10."
+    elsif guess > secret_number
+        puts "Your guess is too high. Try again."
+    elsif guess < secret_number
+        puts "Your guesss is too low. Try again."
+    else puts "Good job!  The secret number was #{secret_number}."
+    end
+    puts "Congratulations!  PRess ENTER to exit."
+    gets
